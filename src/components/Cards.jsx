4015b63ecb8412c130e014634fdd51818de33acc
Alexbv2615 <alexbv2615@gmail.com>
2023-02-21 13:31:00 -0500

    First-Commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,20 @@
+import Card from './Card';
+
+export default function Cards({ characters }) {
+   return (
+      <div>
+         {
+            characters.map(character => {
+               return <Card 
+               key={character.name}
+               name = {character.name}
+               species = {character.species}
+               gender = {character.gender}
+               image = {character.image}
+               onClose ={() => window.alert('Emulamos que se cierra la card')}
+               />
+            })
+         }
+      </div>
+   );
+}
